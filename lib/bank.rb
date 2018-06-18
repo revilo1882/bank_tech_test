8da8534089dcd8ad6e98dbaa399217a2fae86f74
Revilo1882 <oliverrharris@gmail.com>
2018-06-18 15:07:22 +0100

    add withdrawal method

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,4 +3,8 @@ class Bank
   def deposit(amount)
     amount
   end
+
+  def withdrawal(amount)
+    -amount
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,10 +5,14 @@ describe Bank do
   let(:bank) { Bank.new }
 
   describe 'deposit' do
-    it { is_expected.to respond_to(:deposit) }
-
     it 'returns the amount deposited' do
       expect(bank.deposit(10)).to eq(10)
     end
   end
+
+  describe 'withdrawal' do
+    it 'returns the negaitve amount withdrawn' do
+      expect(bank.withdrawal(10)).to eq(-10)
+    end
+  end 
 end
