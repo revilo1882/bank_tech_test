1adafde8c742019f9421a5080ce09dfa16aecc13
Revilo1882 <oliverrharris@gmail.com>
2018-06-19 14:11:00 +0100

    Add attribute reader for statement and balance

--- lib/balance.rb
+++ lib/balance.rb
@@ -1,6 +1,6 @@
 class Balance
 
-  attr_reader :total
+  attr_reader :total, :statement
 
   def initialize(statement = Statement.new)
     @statement = statement
@@ -9,15 +9,15 @@ class Balance
 
   def credit(amount)
     @total += amount
-    @statement.add_credit(amount, @total)
+    statement.add_credit(amount, total)
   end
 
   def debit(amount)
     @total -= amount
-    @statement.add_debit(amount, @total)
+    statement.add_debit(amount, total)
   end
 
   def print_balance
-    @statement.print_transactions
+    statement.print_transactions
   end
 end
--- lib/bank.rb
+++ lib/bank.rb
@@ -1,18 +1,20 @@
 class Bank
 
+  attr_reader :balance
+
   def initialize(balance = Balance.new)
     @balance = balance
   end
 
   def deposit(amount)
-    @balance.credit(amount)
+    balance.credit(amount)
   end
 
   def withdraw(amount)
-    @balance.debit(amount)
+    balance.debit(amount)
   end
 
   def print_statement
-    @balance.print_balance
+    balance.print_balance
   end
 end
